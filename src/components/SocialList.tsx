9b39b94cdb359f7322972072d5a20de95b5d13b5
Benjamin Weinstock <info@flimmerkiste.it>
2021-10-19 19:55:12 +0200

    :heavy_plus_sign: Add styled Components

--- src/components/SocialList.tsx
+++ src/components/SocialList.tsx
@@ -1,7 +1,7 @@
-import React from "react";
-import Twitter from "../assets/twitter-alt.svg";
-import GitHub from "../assets/github-alt.svg";
-import config from "../lib/config";
+import React from 'react'
+import Twitter from '../assets/twitter-alt.svg'
+import GitHub from '../assets/github-alt.svg'
+import config from '../lib/config'
 
 export function SocialList({}) {
   return (
@@ -12,7 +12,7 @@ export function SocialList({}) {
         target="_blank"
         rel="noopener"
       >
-        <Twitter width={24} height={24} fill={"#222"} />
+        <Twitter width={24} height={24} fill={'#222'} />
       </a>
       <a
         title="GitHub"
@@ -20,16 +20,8 @@ export function SocialList({}) {
         target="_blank"
         rel="noopener"
       >
-        <GitHub width={24} height={24} fill={"#222"} />
+        <GitHub width={24} height={24} fill={'#222'} />
       </a>
-      <style jsx>{`
-        a {
-          display: inline-block;
-        }
-        a:not(:last-child) {
-          margin-right: 2em;
-        }
-      `}</style>
     </div>
-  );
+  )
 }
