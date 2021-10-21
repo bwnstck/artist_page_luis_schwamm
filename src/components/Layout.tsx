0daeb894301b0aef04c6d34abe64bca37dcb2d9b
Benjamin Weinstock <info@flimmerkiste.it>
2021-10-21 17:25:47 +0200

    :chart_with_upwards_trend: Add Ackee Analytics

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import Script from 'next/script'
 
 import Navigation from './Navigation'
 
@@ -9,19 +10,27 @@ type Props = {
 }
 export default function Layout({ children }: Props) {
   return (
-    <div className="root">
-      <Head>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="manifest" href="/site.webmanifest" />
-        <link rel="apple-touch-icon" href="/icon.png" />
-        <meta name="theme-color" content="#fff" />
-      </Head>
-      <nav>
-        <Navigation />
-      </nav>
-      <Wrapper>{children}</Wrapper>
-    </div>
+    <>
+      <div className="root">
+        <Head>
+          <meta charSet="utf-8" />
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <link rel="manifest" href="/site.webmanifest" />
+          <link rel="apple-touch-icon" href="/icon.png" />
+          <meta name="theme-color" content="#fff" />
+        </Head>
+        <nav>
+          <Navigation />
+        </nav>
+        <Wrapper>{children}</Wrapper>
+      </div>
+      <Script
+        async
+        src="https://performance.weinstock.it/tracker.js"
+        data-ackee-server="https://performance.weinstock.it"
+        data-ackee-domain-id="cbaeda87-4da7-4211-a0f4-5cca82de0e04"
+      />
+    </>
   )
 }
 
