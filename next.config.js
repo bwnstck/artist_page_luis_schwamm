6b26138eb46ee16c28296e40cb487f872b194c32
Benjamin Weinstock <info@flimmerkiste.it>
2021-10-23 10:11:42 +0200

    :globe_with_meridians: Add lang-attribute to html

--- next.config.js
+++ next.config.js
@@ -5,6 +5,10 @@ module.exports = {
     }
   },
   pageExtensions: ['tsx'],
+  i18n: {
+    locales: ['de'],
+    defaultLocale: 'de',
+  },
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
     config.module.rules.push(
